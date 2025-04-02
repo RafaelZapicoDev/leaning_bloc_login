460a5e80fce8261ecdb8fd48859f0817b6636f4e
rafael <rafaelzapico.ti@gmail.com>
2025-04-02 10:31:42 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# leaning_bloc_login
+
+A new Flutter project.
---
+++ packages/authentication_repository/lib/src/authentication_repository.dart
@@ -0,0 +1,30 @@
+import 'dart:async';
+
+enum AuthenticationStatus { unknown, authenticated, unauthenticated }
+
+class AuthenticationRepository {
+  final _controller = StreamController<AuthenticationStatus>();
+
+  Stream<AuthenticationStatus> get status async* {
+    await Future<void>.delayed(const Duration(seconds: 1));
+    yield AuthenticationStatus
+        .unauthenticated; //delega o estado como não autenticado
+    yield* _controller.stream; //passa a emisão da stream para o controller
+  }
+
+  Future<void> logIn({
+    required String username,
+    required String password,
+  }) async {
+    await Future.delayed(
+      const Duration(milliseconds: 300),
+      () => _controller.add(AuthenticationStatus.authenticated),
+    );
+  }
+
+  void logOut() {
+    _controller.add(AuthenticationStatus.unauthenticated);
+  }
+
+  void dispose() => _controller.close();
+}
